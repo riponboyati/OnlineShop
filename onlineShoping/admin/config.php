533028778d3191998a9f79b1f8cdd07ac951de93
riponboyati <riponboyati@gmail.com>
2016-09-03 22:15:34 +0600

    e commarce project

---
+++ onlineShoping/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/onlineShoping/admin/');
+define('HTTP_CATALOG', 'http://localhost/onlineShoping/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/onlineShoping/admin/');
+define('HTTPS_CATALOG', 'http://localhost/onlineShoping/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/onlineShoping/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/onlineShoping/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/onlineShoping/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/onlineShoping/admin/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/onlineShoping/admin/view/template/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/onlineShoping/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/onlineShoping/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/onlineShoping/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/onlineShoping/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/onlineShoping/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/onlineShoping/system/storage/upload/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/onlineShoping/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'online_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ onlineShoping/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/onlineShoping/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/onlineShoping/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/onlineShoping/catalog/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/onlineShoping/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/onlineShoping/image/');
+define('DIR_LANGUAGE', 'C:/xampp/htdocs/onlineShoping/catalog/language/');
+define('DIR_TEMPLATE', 'C:/xampp/htdocs/onlineShoping/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/xampp/htdocs/onlineShoping/system/config/');
+define('DIR_CACHE', 'C:/xampp/htdocs/onlineShoping/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/xampp/htdocs/onlineShoping/system/storage/download/');
+define('DIR_LOGS', 'C:/xampp/htdocs/onlineShoping/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/xampp/htdocs/onlineShoping/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/xampp/htdocs/onlineShoping/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'online_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
